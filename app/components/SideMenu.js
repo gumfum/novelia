9925edcf93b9538dc17efe88080472db2d8d427f
Kazuma Kitano <kk90.north@gmail.com>
2016-12-31 14:25:03 +0900

    add deawer

---
+++ app/components/SideMenu.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+import { Text } from 'react-native';
+
+class SideMenu extends Component {
+  render () {
+    return (
+      <Text>
+        This is Side Menu.
+      </Text>
+    );
+  }
+}
+
+export default SideMenu;
--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import { connect, Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import Drawer from './components/Drawer'
 import Home from './components/Home'
 
 const RouterWithRedux = connect()(Router);
@@ -20,7 +21,10 @@ class App extends Component {
     return (
       <Provider store={store}>
         <RouterWithRedux>
-          <Scene key="home" component={Home}>
+          <Scene key="drawer" component={Drawer} open={true} >
+            <Scene key="main" tabs={true} >
+              <Scene key="home" component={Home} initial={true} title="Basic" />
+            </Scene>
           </Scene>
         </RouterWithRedux>
       </Provider>
