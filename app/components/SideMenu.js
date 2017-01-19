2c266e0440612485db4efa4ec0a15f41fed10e3c
Ryohei Takasawa <henshinfukanou@gmail.com>
2017-01-20 01:34:50 +0900

    Add ListView sample

--- app/components/SideMenu.js
+++ app/components/SideMenu.js
@@ -1,12 +1,21 @@
 import React, { Component, PropTypes } from 'react';
-import { Text } from 'react-native';
+import { Text, ListView } from 'react-native';
 
 class SideMenu extends Component {
+  constructor() {
+  	super();
+  	const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  	this.state = {
+  	  dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+  	}
+  }
+
   render () {
     return (
-      <Text>
-        This is Side Menu.
-      </Text>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
     );
   }
 }
